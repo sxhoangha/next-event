d6b57742ccd2f16ddde452484e2a59e45f92f47c
Hoang Ha <halehoang@gmail.com>
2021-03-31 17:38:59 -0400

    working on event routes and items

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,27 @@
 import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function EventDetailPage() {
   const router = useRouter();
-  console.log(router.query);
-  return (
-    <div>
-      <h2>This show a specific event</h2>
-    </div>
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  return event ? (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  ) : (
+    <p>No event found!</p>
   );
 }
