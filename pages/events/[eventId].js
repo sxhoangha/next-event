b5ef879125c798997e93c599fcf6243c919f713d
Hoang Ha <halehoang@gmail.com>
2021-04-05 12:12:54 -0400

    working on Home, EventDetail and AllEvents pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,14 +1,14 @@
-import { useRouter } from "next/router";
-import { getEventById } from "../../dummy-data";
+import {
+  getEventById,
+  getAllEvents,
+  getFeaturedEvents,
+} from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
 
-export default function EventDetailPage() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+export default function EventDetailPage({ event }) {
   return event ? (
     <>
       <EventSummary title={event.title} />
@@ -23,8 +23,29 @@ export default function EventDetailPage() {
       </EventContent>
     </>
   ) : (
-    <ErrorAlert>
-      <p>No event found!</p>
-    </ErrorAlert>
+    <div className="center">
+      <p>Loading...</p>
+    </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+  return {
+    props: {
+      event: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents(); //tell nextJS to only pre-render featured events instead of all events to have better performance
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    fallback: "blocking", //false:if user browse to a eventId that not exist at build time, nextJS will show 404 page even there is that event at current time
+    //true: tell nextJS that we might have more pages than what we have a  build time
+  };
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,10 @@
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-export default function EventsPage() {
+export default function EventsPage({ allEvents }) {
   const router = useRouter();
-  const events = getAllEvents();
-
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
@@ -15,7 +13,17 @@ export default function EventsPage() {
   return (
     <>
       <EventsSearch onSearch={findEventsHandler} />
-      <EventList items={events} />
+      <EventList items={allEvents} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      allEvents: events,
+    },
+    revalidate: 60,
+  };
 }
