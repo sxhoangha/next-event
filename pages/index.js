7edffa59c158bac26c11ae174134e4d4a472c9e7
Hoang Ha <halehoang@gmail.com>
2021-04-06 12:48:58 -0400

    some optimization added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../helpers/api-util";
+import Head from "next/head";
 
 export default function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of events that allow you to develop yourself"
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
