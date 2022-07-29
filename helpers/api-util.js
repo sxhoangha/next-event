d2ccd60e30cbc00c4249352caefffd8b4f425b04
Hoang Ha <hha@opentext.com>
2022-07-29 12:10:20 -0400

    using dummy data as firebase stop working

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -22,10 +22,49 @@ export async function getFilteredEvents(dateFilter) {
 }
 
 export async function getAllEvents() {
-  const response = await fetch(
-    "https://kanbanfire-12f4e-default-rtdb.firebaseio.com/events.json"
-  );
-  const data = await response.json();
+  // const response = await fetch(
+  //   "https://kanbanfire-12f4e-default-rtdb.firebaseio.com/events.json"
+  // );
+  // const data = await response.json();
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  console.log("start fetching");
+  await delay(1000);
+  console.log("end fetching");
+  const data = [
+    {
+      id: "e1",
+      title: "Programming for everyone",
+      description:
+        "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
+      location: "Somestreet 25, 12345 San Somewhereo",
+      date: "2021-05-12",
+      image: "images/coding-event.jpg",
+      isFeatured: false,
+      key: "e1",
+    },
+    {
+      id: "e2",
+      title: "Networking for introverts",
+      description:
+        "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+      location: "New Wall Street 5, 98765 New Work",
+      date: "2021-05-30",
+      image: "images/introvert-event.jpg",
+      isFeatured: true,
+      key: "e2",
+    },
+    {
+      id: "e3",
+      title: "Networking for extroverts",
+      description:
+        "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
+      location: "My Street 12, 10115 Broke City",
+      date: "2022-04-10",
+      image: "images/extrovert-event.jpg",
+      isFeatured: true,
+      key: "e3",
+    },
+  ];
   const events = [];
   for (const key in data) {
     events.push({
